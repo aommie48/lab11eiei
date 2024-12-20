5e9e7497d3eb6685da3a3095219ea04bd3a3f63b
lab11eiei <ananya.aomsinxz@gmail.com>
2024-12-20 14:50:55 +0700

    edit

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future.";
+
+    for (int i = 0; i < 3; i++) {
+        cin.ignore();
+    }
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int randomIndex = rand() % 9;
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102.";
+
+    return 0;
+}
